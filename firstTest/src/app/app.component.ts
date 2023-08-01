48959d3b3e6e9f54f7600f05949204cd43b362c1
Ilya Quateau [Kapernikov] <ilya.quateau@kapernikov.com>
2023-08-01 16:27:52 +0200

    testing variable layout and services

--- firstTest/src/app/app.component.ts
+++ firstTest/src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { Logger } from './logger.service'; // Import services
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [Logger] // Provide services to use them
 })
 export class AppComponent {
+  constructor(
+    private logger: Logger // Services in constructor to initialise
+  ) {}
+
   title: string = 'firstTest';
 
   onClickMe(a: string) {
-    this.title = a;
+    this.title = a ? a : "No title";
   }
 
   desc: string = 'Empty'
@@ -18,5 +24,16 @@ export class AppComponent {
   onKey(event: KeyboardEvent) {
     const target: HTMLInputElement = event.target as HTMLInputElement
     this.desc = target.value ? target.value : "Empty" ;
+    this.logger.log(this.desc) // Call services from constructor
   }
+
+  val: number = 0
+
+  inc() {this.calc(+1)}
+  dec() {this.calc(-1)}
+  calc(delta: number) {
+    this.val += delta
+  }
+
+  arr: Array<string> = ["This", "is", "a", "test"];
 }
